6a5636149510117689cd647e01ba509454f22045
Nicholas Davies <nicholasjohndavies42@gmail.com>
2020-03-23 21:19:07 +0800

    Fridge Buttons added rmove function

--- Fridge_Content/Fridge.js
+++ Fridge_Content/Fridge.js
@@ -1,31 +1,71 @@
-var ingredients = ["Eggs", "Onions", "Chocolate"];
+//sets the ingredients array
+var ingredients = [];
 
+//checks if anything is locally stored
+if (localStorage.getItem("Ingredients") !== null) {
+    ingredients = JSON.parse(localStorage.getItem("Ingredients"));
+};
+
+// renders the elements
+function RenderElements() {
+    $("#listIngredients").empty();
+    for(i = 0; i < ingredients.length; i++) {
+        var ingredient = ingredients[i];
+        //creates necessary elements
+        var li = $("<li>");
+        var div = $("<div>");
+        var a = $("<a>");
+
+        // adds necessary classes and ids
+        a.addClass("secondary-content waves-effect waves-light removeBtn");
+        li.addClass("collection-item");
+        a.attr("id", ingredient);
+        // sets text of elements
+        a.text("Remove");
+        div.text(ingredient);
+
+        // appends elements
+        div.append(a);
+        li.append(div);
+
+        // sets text to input
+        $("#listIngredients").append(li);
+    }
+}
+
+//when the add button is pushed
 $("#ingredientsInput").on("click", function() {
-    //creates necessary elements
-    var li = $("<li>");
-    var p = $("<p>");
-    var div = $("<div>");
-    var a = $("<a>");
+    var ingredient = $("#ingredients").val();
+    // if the ingredient is already in the array
+    if(ingredients.indexOf(ingredient) !== -1) {
+        //alert("") <---- need to use modal window
+    } else {
+        var ingredient = $("#ingredients").val();
+        
+        //resets the ingredient input
+        $("#ingredients").val("");
 
-    // adds necessary classes
-    a.addClass("secondary-content waves-effect waves-light removeBtn");
-    li.addClass("collection-item");
-    // a.attr();
+        //adds ingredient to the array
+        ingredients.push(ingredient);
 
-    // sets text of elements
-    a.text("Remove");
-    div.text($("#ingredients").val());
+        //stores the new array locally
+        localStorage.setItem("Ingredients", JSON.stringify(ingredients));
 
-    // appends elements
-    div.append(a);
-    li.append(div);
-
-    // sets text to input
-    $("#listIngredients").append(li);
-    $("#ingredients").val("");
+        // renders elements
+        RenderElements();
+    }
 });
 
 $(document).on("click", ".removeBtn", function() {
-    var div = $(this).parent();
-    div.parent().remove();
+    //removes from array
+    var indexOfIngredient = ingredients.indexOf($(this).attr("id"));
+    ingredients.splice(indexOfIngredient, 1);
+
+    //stores the new array locally 
+    localStorage.setItem("Ingredients", JSON.stringify(ingredients));
+
+    //renders the array
+    RenderElements();
 });
+
+RenderElements();
