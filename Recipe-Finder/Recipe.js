6853d4806c65b5ef040d1bd3dcf9b5662a62d100
OrbmasWin <sambrostudent@gmail.com>
2020-03-29 14:17:35 +0800

    added basic spoonacular api call and return

--- Recipe-Finder/Recipe.js
+++ Recipe-Finder/Recipe.js
@@ -10,13 +10,65 @@ searchBtn.addEventListener("click", function () {
 function getRecipe(searchValue) {
     $.ajax({
         type: "GET",
-        url: "https://api.spoonacular.com/recipes/search?query=" + searchValue + "&number=6&apiKey=6700e89d83964e28b700a8b597b8123c",
+        url: "https://api.spoonacular.com/recipes/complexSearch?query=" + searchValue + "&addRecipeInformation=True&fillIngredients=True&number=2&apiKey=6700e89d83964e28b700a8b597b8123c",
         dataType: "json",
         success: function (data) {
-            var result = data.results[0].title
-            recipeResult.append(result)
+            for (i = 0; i < data.results.length; i++) {
+                //create div to store results
+                var resultDiv = document.createElement("div");
+                resultDiv.classList.add("result-div-styles", "row")
 
-            console.log(result);
+                // call title
+                var recipeTitleEl = document.createElement("h5");
+                var resultTitle = data.results[i].title;
+                recipeTitleEl.append(resultTitle)
+
+                //call prep time
+                var resultPrepTime = data.results[i].preparationMinutes;
+                var prepTimeEl = document.createElement("p");
+                prepTimeEl.append("Preparation time: " + resultPrepTime + " minutes")
+
+                //call cooking time
+                var resultCookingTime = data.results[i].cookingMinutes;
+                var cookingTimeEl = document.createElement("p");
+                cookingTimeEl.append("Cooking time: " + resultCookingTime + " minutes")
+
+                //call serving size 
+                var resultServingSize = data.results[i].servings;
+                var servingSizeEl = document.createElement("p");
+                servingSizeEl.append("Serving Size: " + resultServingSize)
+
+                //call image
+                var recipeImg = document.createElement("img")
+                recipeImgLink = data.results[i].image;
+                recipeClass = "." + data.results[i].id;
+                recipeImg.classList.add(recipeClass, "recipeImgStyles");
+                recipeImg.setAttribute('src', recipeImgLink)
+
+
+                //append 
+                recipeResult.append(resultDiv)
+                resultDiv.append(recipeImg)
+                resultDiv.append(recipeTitleEl)
+                resultDiv.append(cookingTimeEl)
+                resultDiv.append(prepTimeEl)
+                resultDiv.append(servingSizeEl)
+
+                //call ingredients
+                for (j = 0; j < data.results[i].missedIngredients.length; j++) {
+                    var ingredientName = data.results[i].missedIngredients[j].name
+                    var ingredientDiv = document.createElement("div");
+
+                    var ingredientEl = document.createElement("p");
+                    ingredientEl.append(ingredientName)
+                    console.log(data.results[i].missedIngredients[j].name);
+
+                    ingredientDiv.append(ingredientEl)
+                    resultDiv.append(ingredientDiv)
+                }
+
+            }
+
 
         }
     })
