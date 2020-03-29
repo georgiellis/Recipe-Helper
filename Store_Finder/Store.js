f91e80742726c19f207819c0e7240ef7726a5ff5
Georgie <georginakellis@gmail.com>
2020-03-29 19:01:34 +0800

    html updated, api calls now functioning. Table production still needs work

--- Store_Finder/Store.js
+++ Store_Finder/Store.js
@@ -1,57 +1,95 @@
 // api key: 36838fddf630f5d0a43b9234ea3eb50a
-// search function
 
-// to fetch location id
+// on click
 
-function cityID(cityName) {
-  var apiKey = "36838fddf630f5d0a43b9234ea3eb50a";
-  var cityName = document.getElementById("city-name").value;
-  var queryIDUrl =
-    "https://developers.zomato.com/api/v2.1/locations?query=" + cityName;
-
-  $.ajax({
-    url: queryIDUrl,
-    headers: { "user-key": apiKey },
-    dataType: "json",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
-
-  //creating html for restaurant results
-
-  //creating div to store 5 or so restaurant results
-
-  //creating tags for the result information we want to display
-
-  //
-}
-
-// function to fetch restaurants based of location id
-
-function findRestaurant() {
-  var locationID = response.location_suggestions[0].entity_id;
-  var queryUrl =
-    "https://developers.zomato.com/api/v2.1/search?query=" + locationID;
-
-  $.ajax({
-    url: queryUrl,
-    headers: { "user-key": apiKey },
-    dataType: "json",
-    method: "GET"
-  }).then(function(response) {});
-}
-
-//grab city data from submit button function
-
-$("#add-city").on("click", function(event) {
-  event.preventDefault();
-
-  var cityInput = $("#city-name").val();
-
-  cityID(cityInput);
+$("#findRestaurants").on("click", function(event) {
+    event.preventDefault();
+    // runs findRestaurants function
+    findLocation();
 });
 
-// calling cityID function
+// find location function
 
-cityID();
+function findLocation() {
+
+     //first ajax call to get city ID from Zomato
+     var search = $("#restaurants").val().trim();
+     var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=" + search;
+     $.ajax({
+         url: queryURL,
+         method: "GET",
+         headers: {"user-key": "36838fddf630f5d0a43b9234ea3eb50a"},
+         dataType: "json",
+     })
+     .then(function(response){
+        console.log(response);
+        console.log(response.location_suggestions[0].id);
+    
+        //second call to get the actual info that I want using Zomato ID
+        var searchID = response.location_suggestions[0].id;
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + searchID;
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            headers: {"user-key": "36838fddf630f5d0a43b9234ea3eb50a"},
+            dataType: "json",
+        })
+        .then(function(response2){
+            console.log(response2);
+    
+        });
+    });
+}
+
+var getRestaurant = function() {
+
+    // creating variables for the 2 text fields
+    var locationInput = $("#location-input").val();
+    
+    // test
+    console.log(locationInput);
+
+    // clearing the text field on submit
+    $("#location-input").val("");
+
+    var queryURL = "https://developers.zomato.com/api/v2.1/cities?q="+ locationInput;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        headers: {"user-key": "36838fddf630f5d0a43b9234ea3eb50a"},
+        dataType: "json",
+    })
+    .then(function(response){
+        console.log(response);
+
+      // variables for the query responses we want
+      var name = response.name;
+      var location = response.location;
+      var pics = response.thumb;
+      var cuisine = response.cuisine;
+      var userRating = response.user_rating;
+      var phoneNumber = response.phone_numbers;
+      
+      // appending the results to the table
+      $("#restaurant-table > tbody").append(
+      "<tr><td id='table-name'>" + name + 
+      "</td><td id='table-location'>" + location + 
+      "</td><td id='table-pics'>" + pics + 
+      "</td><td id='table-cuisine'>" + cuisine +  
+      "</td><td id='table-rating'>" + userRating + 
+      "</td><td id='table-phone'>" + phoneNumber + 
+      "</td></tr>");
+
+    });
+};
+
+$("#findRestaurants").on("click", function(event) {
+
+    event.preventDefault();
+    getRestaurant()
+
+
+});
+
+
